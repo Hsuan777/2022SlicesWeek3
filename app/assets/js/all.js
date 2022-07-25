3bc2f2e42e4e92d388ed08cfa940bdb55aeb6e44
Vic <syuan0212@gmail.com>
2022-07-25 21:36:15 +0800

    fix: checkbox

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -3,5 +3,9 @@ console.log('Hello!');
 $(document).ready(() => {
   $('#menu-mobile-btn').click(() => {
     $('#menu-mobile').toggleClass('d-none');
+  });
+  $('#agree').click(() => {
+    $('#agreeFalse').toggleClass('d-none');
+    $('#agreeTrue').toggleClass('d-none');
   })
 });
